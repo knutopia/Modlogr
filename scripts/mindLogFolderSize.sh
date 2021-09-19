d821898ac9ea4522f3d21aa05bb98b1d1e603533
knutopia <knutopia@gmail.com>
2021-09-19 17:28:34 -0500

    log minding more

--- scripts/mindLogFolderSize.sh
+++ scripts/mindLogFolderSize.sh
@@ -3,14 +3,13 @@
 folder_path="/home/pi/Modlogr/logs"
 log_file="/home/pi/Modlogr/logs/logster.log"
 id="mindLogFolderSize.sh"
-echo -n `date '+%F_%H:%M:%S'` $id >>$log_file
+echo `date '+%F_%H:%M:%S'` $id >>$log_file
 
 size=$(/usr/bin/du -s $folder_path  | /usr/bin/awk '{print $1}')
 echo "$folder_path  -  ${size}kB" >>$log_file
 
 if [[ $size -gt $(( 1024 )) ]]; then
-  echo -n "truncating" >>$log_file
   sudo truncate -c --size='<'100K /home/pi/Modlogr/logs/*.log
-  echo %? >>$log_file
+  echo -n $id "truncated" %? >>$log_file
 fi
 
